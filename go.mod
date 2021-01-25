8b769727257b94e01315076918465d64003e2c75
Stepan Vrany <stepan@vrany.dev>
2021-01-25 20:36:24 +0100

    add automation for verify service

--- go.mod
+++ go.mod
@@ -5,7 +5,6 @@ go 1.16
 require (
 	github.com/aws/aws-lambda-go v1.22.0
 	github.com/aws/aws-sdk-go v1.36.31
-	github.com/go-playground/locales v0.13.0
 	github.com/sirupsen/logrus v1.7.0
 	golang.org/x/sys v0.0.0-20201009025420-dfb3f7c4e634 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
--- internal/handler/verify.go
+++ internal/handler/verify.go
@@ -4,6 +4,8 @@ import (
 	"context"
 
 	"github.com/aws/aws-lambda-go/events"
+	log "github.com/sirupsen/logrus"
+	"github.com/vranystepan/email/pkg/messages/issue"
 	"github.com/vranystepan/email/pkg/service"
 )
 
@@ -15,6 +17,18 @@ type VerifyParams struct {
 // Verify sends verification email via SES
 func Verify(p VerifyParams) func(ctx context.Context, sqsEvent events.SQSEvent) error {
 	return func(ctx context.Context, sqsEvent events.SQSEvent) error {
+		for _, message := range sqsEvent.Records {
+			payload, err := issue.Parse(message.Body)
+			if err != nil {
+				log.
+					WithField("error", err).
+					Error("could not parse incoming message")
+				return err
+			}
+			log.
+				WithField("email", payload.Email).
+				Info("processing payload")
+		}
 		return nil
 	}
 }

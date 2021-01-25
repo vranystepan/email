defd29b71b378dedbee8e06f33ac3628460cf86d
Stepan Vrany <stepan@vrany.dev>
2021-01-25 20:25:01 +0100

    debug missing email field

--- pkg/messages/issue/issue.go
+++ pkg/messages/issue/issue.go
@@ -4,7 +4,10 @@ import (
 	"encoding/json"
 	"time"
 
-	"github.com/vranystepan/email/pkg/messages/message"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/sqs"
+	log "github.com/sirupsen/logrus"
+	"github.com/vranystepan/email/pkg/service"
 )
 
 // New creates a new Payload struct
@@ -17,7 +20,6 @@ func New(email string) Payload {
 
 // Payload holds registration data
 type Payload struct {
-	message.Payload
 	Email       string    `json:"email"`
 	TimeCreated time.Time `json:"timeCreated"`
 }
@@ -30,4 +32,33 @@ func Parse(message string) (Payload, error) {
 		return m, err
 	}
 	return m, nil
+}
+
+// JSON converts RegisterPayload data to JSON string
+func (r Payload) JSON() (string, error) {
+	b, err := json.Marshal(r)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
+}
+
+// Send sends message to the given SQS queue
+func (r Payload) Send(svc service.SQS, queue string) error {
+	// convert body to JSON
+	body, err := r.JSON()
+	if err != nil {
+		return err
+	}
+
+	// send message to message broker
+	message := &sqs.SendMessageInput{
+		//MessageAttributes: map[string]*sqs.MessageAttributeValue{},
+		MessageBody: aws.String(body),
+		QueueUrl:    aws.String(queue),
+	}
+	log.WithField("payload", r).Info("sending message")
+
+	_, err = svc.SendMessage(message)
+	return err
 }
--- pkg/messages/register/register.go
+++ pkg/messages/register/register.go
@@ -4,12 +4,14 @@ import (
 	"encoding/json"
 	"time"
 
-	"github.com/vranystepan/email/pkg/messages/message"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/sqs"
+	log "github.com/sirupsen/logrus"
+	"github.com/vranystepan/email/pkg/service"
 )
 
 // Payload holds registration data
 type Payload struct {
-	message.Payload
 	TimeCreated time.Time `json:"timeCreated"`
 	Source      string    `json:"source"`
 	Email       string    `json:"email"`
@@ -23,4 +25,33 @@ func Parse(message string) (Payload, error) {
 		return m, err
 	}
 	return m, nil
+}
+
+// JSON converts RegisterPayload data to JSON string
+func (r Payload) JSON() (string, error) {
+	b, err := json.Marshal(r)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
+}
+
+// Send sends message to the given SQS queue
+func (r Payload) Send(svc service.SQS, queue string) error {
+	// convert body to JSON
+	body, err := r.JSON()
+	if err != nil {
+		return err
+	}
+
+	// send message to message broker
+	message := &sqs.SendMessageInput{
+		//MessageAttributes: map[string]*sqs.MessageAttributeValue{},
+		MessageBody: aws.String(body),
+		QueueUrl:    aws.String(queue),
+	}
+	log.WithField("payload", r).Info("sending message")
+
+	_, err = svc.SendMessage(message)
+	return err
 }
